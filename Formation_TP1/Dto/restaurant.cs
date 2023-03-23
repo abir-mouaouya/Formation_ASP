09d6e830964f118bd2d91bd242edb9bce5f3c8ca
Abir Mouaouya <abirmouaouya2001@gmail.com>
2023-03-23 09:19:11 +0100

    second

--- Formation_TP1/Dto/restaurant.cs
+++ Formation_TP1/Dto/restaurant.cs
@@ -1,10 +1,15 @@
-﻿namespace Formation_TP1.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formation_TP1.Dto
 {
     public class restaurant
     {
         public string Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string cuisine { get; set; }
 
 
---
+++ Formation_TP1/helper/helpers.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Formation_TP1.helper
+{
+    public class helpers :  TagHelper   
+    {
+        public helpers() { }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+        }
+    }
+}
